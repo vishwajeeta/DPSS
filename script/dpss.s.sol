1a891dfeee103c059ec652fd5cf28e85e49b34e1
vishwajeeta <gs699542@gmail.com>
2025-03-26 17:31:04 +0530

    modified and added gas report

---
+++ script/dpss.s.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.17;
+import {Script} from "forge-std/Script.sol";
+import {SecureIPFSStorage} from "../src/dpss.sol";
+contract DeploySecureIPFSStorage is Script {
+    function run()external {
+        vm.startBroadcast();
+        new SecureIPFSStorage();
+        vm.stopBroadcast();
+    }
+}
