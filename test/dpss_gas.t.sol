902217a33521d1666547f9c8d6e66bd122c59313
vishwajeeta <gs699542@gmail.com>
2025-03-26 18:06:14 +0530

    done more tests on contracts both gas and normal

---
+++ test/dpss_gas.t.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.17;
+
+import {Test, console} from "forge-std/Test.sol";
+import {SecureIPFSStorage} from "../src/dpss_gas.sol";
+
+contract SecureIPFSStorage_gas_Test is Test {
+    SecureIPFSStorage public sipfss;
+
+    function setUp() public {
+        sipfss = new SecureIPFSStorage();
+        sipfss.storeDocument("1111111111111111111111111111111111111111111111","sample1");
+    }
+    function test_storeDocument() public {
+        sipfss.storeDocument("1111111111111111111111111111111111111111111111","sample1");
+    }
+    function test_deleteDocument() public {
+        sipfss.deleteDocument(0);
+    }
+    function test_getDocumentCount() public {
+        sipfss.getDocumentCount();
+        assertEq(sipfss.getDocumentCount(),1);
+    }
+    function test_getDocument() public {
+        sipfss.getDocument(0);
+    }
+}
